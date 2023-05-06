44d6d9e4b6258701a3bf243c84da9a0c093c3f24
Veith M. Bürgerhoff <veith.buergerhoff@gmail.com>
2023-05-06 11:59:28 +0200

    finish basic player controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +8,21 @@ public class PlayerController : MonoBehaviour
 {
     public float PlayerSpeed = 7;
     public float Gravity = 6;
+    [Range(0, 1)]
+    public float RotationSpeed = 0.15f;
+    public Transform RotatingTransform;
+
     private CharacterController _characterController;
-    private float _verticalSpeed = 6;
+    private float _verticalSpeed;
 
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
+
+        if (RotatingTransform is null)
+        {
+            Debug.LogError($"{nameof(RotatingTransform)} must be set");
+        }
     }
 
     void Update()
@@ -24,6 +34,12 @@ public class PlayerController : MonoBehaviour
     {
         var horizontalMovement = transform.right * Input.GetAxis("Horizontal");
         var verticalMovement = transform.forward * Input.GetAxis("Vertical");
+        var playerMovement = verticalMovement + horizontalMovement;
+
+        if (playerMovement != Vector3.zero)
+        {
+            RotatingTransform.rotation = Quaternion.Slerp(RotatingTransform.rotation, Quaternion.LookRotation(playerMovement), RotationSpeed);
+        }
 
         if (_characterController.isGrounded)
         {
@@ -34,8 +50,9 @@ public class PlayerController : MonoBehaviour
             _verticalSpeed -= Gravity * Time.deltaTime;
         }
 
-        var playerMovement = verticalMovement + horizontalMovement;
-        var movement = PlayerSpeed * Time.deltaTime * playerMovement + new Vector3(0, _verticalSpeed, 0) * Time.deltaTime;
-        _characterController.Move(movement);
+
+        var finalMovement = PlayerSpeed * Time.deltaTime * playerMovement
+            + new Vector3(0, _verticalSpeed, 0) * Time.deltaTime;
+        _characterController.Move(finalMovement);
     }
 }
